ef632f6b2f875e2f968997e7d2d8ca9cd2455e6d
Mahesh Kumar Panda <maheshkpanda24@gmail.com>
2026-09-02 06:25:08 +0530

    config.py file groq model updated

--- app/config.py
+++ app/config.py
@@ -20,7 +20,7 @@ class Settings:
 
     # --- LLM (Groq) ---
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    GROQ_MODEL = "llama-3.3-70b-versatile"
+    GROQ_MODEL = "openai/gpt-oss-120b"
 
 
     # --- Observability ---
